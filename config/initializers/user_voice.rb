e7d59c3f48e6a8e8a088182f06fd0540513ea504
Alban Peignier <alban@tryphon.eu>
2010-03-20 18:31:39 +0100

    Integrate UserVoice plugin for StreamBox forum

---
+++ config/initializers/user_voice.rb
@@ -0,0 +1,10 @@
+UserVoice.config do |account|
+  account.key = 'tryphon'
+  account.host = 'tryphon.uservoice.com'
+  account.forum = '46949' 
+  account.alignment = 'left'
+  account.background_color = '#AE0000' 
+  account.text_color = 'white'
+  account.hover_color = '#D06B6B'
+  account.lang = 'fr'
+end
