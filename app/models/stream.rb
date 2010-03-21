ebc9726eb055c610777910222efdc5f37d76d00f
Alban Peignier <alban@tryphon.eu>
2010-03-21 14:03:13 +0100

    Add validations on Stream model. Fixes #2

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,9 +1,13 @@
 class Stream < ActiveForm::Base
   include PuppetConfigurable
+  include ActsAsIpPort
+  include HostValidation
 
-  attr_accessor :server, :port, :password, :mount_point
+  attr_accessor :server, :password, :mount_point
+  acts_as_ip_port :port
 
-  validates_presence_of :server
+  validates_presence_of :server, :port, :mount_point, :password
+  validates_host :server
 
   def new_record?
     false
--- spec/models/stream_spec.rb
+++ spec/models/stream_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe Stream do
+
   before(:each) do
     @stream = Stream.new
   end
 
-  it "should not allow a blank server" do
-    @stream.should_not allow_values_for(:server, "") 
+  it { should validate_presence_of :server }
+  it { should validate_presence_of :port }
+  it { should validate_presence_of :mount_point }
+  it { should validate_presence_of :password }
+
+  it "should validate server as a host" do
+    @stream.server = "localhost"
+    @stream.stub!(:validate_host).and_return(false)
+    @stream.should have(1).error_on(:server)
   end
 
 end
