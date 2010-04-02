9ee15c0ceb20a87ada7e30d9c19abe350606f109
Alban Peignier <alban@tryphon.eu>
2010-04-02 19:15:35 +0200

    Specify gemcutter source for will_paginate. Refs #8

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem "inherited_resources", :version => "1.0.3"
-  config.gem 'will_paginate', :version => '~> 2.3.11'
+  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
