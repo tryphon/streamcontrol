631f19f90ee9abc7a21dbe9d66575f11638e2956
Florent Peyraud <florent@tryphon.eu>
2010-05-03 20:17:54 +0200

    Added stream details for darkice metadata

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -4,7 +4,7 @@ class Stream < ActiveForm::Base
 
   strip_attributes!
 
-  attr_accessor :name, :id
+  attr_accessor :name, :id, :description, :genre, :related_url
   validates_presence_of :name
 
   attr_accessor :server, :password, :mount_point
