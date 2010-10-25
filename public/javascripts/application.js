86c048d7d0e7242da98061a68fb0b769d8bfd708
Alban Peignier <alban@tryphon.eu>
2010-10-25 19:22:14 +0200

    Disable Stream#mount_point in /streams/_form for shoutcast servers. Refs #19

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,21 @@
+// Disable Stream#mount_point in /streams/_form for shoutcast servers
+Event.observe(window, "load", function() {
+  var toggle_stream_mount_point = function(enable) {
+    $$('input[name="stream[mount_point]"]').each(function(input) {
+      console.log(input);
+      if (enable) {
+        input.enable();
+      } else {
+        input.disable();
+      };
+    });
+  };
 
+  $$('select[name="stream[server_type]"]').each(function(select) {
+    console.log(select.value);
+    toggle_stream_mount_point(select.value != "shoutcast");
+    Event.observe(select, 'change', function() {
+      toggle_stream_mount_point(select.value != "shoutcast");
+    });
+  });
+});
