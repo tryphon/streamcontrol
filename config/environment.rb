2ec05066d99d444b6ab225b524a8b1a7e8239424
Alban Peignier <alban@tryphon.eu>
2012-07-05 14:34:23 +0200

    Add workaround to support 'recent' rubygems

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Be sure to restart your server when you modify this file
 
+if Gem::VERSION >= "1.3.6" 
+    module Rails
+        class GemDependency
+            def requirement
+                r = super
+                (r == Gem::Requirement.default) ? nil : r
+            end
+        end
+    end
+end
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
