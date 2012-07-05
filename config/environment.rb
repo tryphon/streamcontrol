c06aa831203960fd47d1cafd5ecf04216f6cd5f0
Alban Peignier <alban@tryphon.eu>
2012-07-05 14:57:54 +0200

    Patch Rails::GemDependency .. when loaded

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Be sure to restart your server when you modify this file
 
+# Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
+
 if Gem::VERSION >= "1.3.6" 
     module Rails
         class GemDependency
@@ -10,12 +16,6 @@ if Gem::VERSION >= "1.3.6"
         end
     end
 end
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
