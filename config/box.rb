3da73c95e466c55b24989fa9c4435f60d62df8d0
Alban Peignier <alban@tryphon.eu>
2014-08-14 19:06:19 +0200

    Define Box::PuppetConfiguration.configuration_file

--- config/box.rb
+++ config/box.rb
@@ -15,5 +15,7 @@ Box::Release.latest_url = "http://dev.tryphon.priv/dist/streambox/latest.yml"
 Box::Release.current_url = "public/current.yml"
 Box::Release.install_command = "/bin/true"
 
+Box::PuppetConfiguration.configuration_file = "tmp/config.pp"
+
 # Use this script to initialize Box commands executed in background
 Box.start_default_options = { :config => __FILE__ }
